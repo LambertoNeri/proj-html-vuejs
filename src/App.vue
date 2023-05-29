699a7c78c3dae6b23711c388abb521b79d08b67f
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-29 12:38:35 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppFooter.Vue';
+import AppProducts from './components/AppFooter.Vue';
+import AppFooter from './components/AppFooter.Vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppProducts,
+    AppFooter,
+  },
+}
+</script>
+
+<template>
+  <AppHeader/>
+  <AppProducts/>
+  <AppFooter/>
+</template>
+
+<style lang="scss">
+  @use '../src/assets/styles/general.scss' as *;
+</style>
