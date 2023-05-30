dd55eb6399f84c4f5ea834e82d59f0e05e491f92
LambertoNeri <neri.lamberto91@gmail.com>
2023-05-30 15:49:35 +0200

    add AppSectionOne + Two

---
+++ src/components/AppSectionOne.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        data() {
+            return{
+
+            };
+        },
+    }
+</script>
+
+<template>
+    <div>
+        appSectionOne
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
